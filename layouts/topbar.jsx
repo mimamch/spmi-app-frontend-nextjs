1ae2f0aa8cba312978ea18ad88ecc6ad88ce1fa9
mimamch <mimamch28@gmail.com>
2022-08-30 16:32:24 +0700

    update bug

--- layouts/topbar.jsx
+++ layouts/topbar.jsx
@@ -38,59 +38,49 @@ export default function Topbar(props) {
         {/* <!-- Nav Item - Search Dropdown (Visible Only XS) --> */}
 
         {/* <!-- Nav Item - User Information --> */}
-        {getMe.user.username ? (
-          <li className="nav-item dropdown no-arrow">
-            <a
-              className="nav-link dropdown-toggle"
-              href="#"
-              id="userDropdown"
-              role="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              <span className="mr-2 d-none d-lg-inline text-gray-600 small">
-                {getMe.user.fullName} | {getMe.user.role}
-              </span>
-              <img
-                className="img-profile rounded-circle"
-                src="/assets/img/undraw_profile.svg"
-              />
-            </a>
-            {/* <!-- Dropdown - User Information --> */}
-            <div
-              className="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-              aria-labelledby="userDropdown"
-            >
-              <Link href="/profile">
-                <a className="dropdown-item">
-                  <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Profile
-                </a>
-              </Link>
-              <div className="dropdown-divider"></div>
-              <a
-                // onClick={signOut}
-                className="dropdown-item"
-                onClick={logOut}
-                style={{ cursor: "pointer" }}
-                href="#"
-              >
-                <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                Logout
+
+        <li className="nav-item dropdown no-arrow">
+          <a
+            className="nav-link dropdown-toggle"
+            href="#"
+            id="userDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <span className="mr-2 d-none d-lg-inline text-gray-600 small">
+              {getMe?.user?.fullName} | {getMe?.user?.role}
+            </span>
+            <img
+              className="img-profile rounded-circle"
+              src="/assets/img/undraw_profile.svg"
+            />
+          </a>
+          {/* <!-- Dropdown - User Information --> */}
+          <div
+            className="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+            aria-labelledby="userDropdown"
+          >
+            <Link href="/profile">
+              <a className="dropdown-item">
+                <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                Profile
               </a>
-            </div>
-          </li>
-        ) : (
-          <li className="nav-item align-items-center row mx-3">
-            <a href="/login" className="btn btn-dark btn-sm btn-icon-split">
-              <span className="icon text-gray-600">
-                <i className="fas fa-sign-in-alt"></i>
-              </span>
-              <span className="text">Masuk</span>
+            </Link>
+            <div className="dropdown-divider"></div>
+            <a
+              // onClick={signOut}
+              className="dropdown-item"
+              onClick={logOut}
+              style={{ cursor: "pointer" }}
+              href="#"
+            >
+              <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+              Logout
             </a>
-          </li>
-        )}
+          </div>
+        </li>
       </ul>
     </nav>
   );
